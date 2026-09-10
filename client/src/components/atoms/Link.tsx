9b876275b0645f413fa3912bda973a942e9a8197
vladyslavspasibozhko <>
2026-09-10 19:11:37 +0200

    feat: update Link styles

--- client/src/components/atoms/Link.tsx
+++ client/src/components/atoms/Link.tsx
@@ -1,13 +1,13 @@
-import type { ReactNode } from 'react';
+import type { ReactNode } from "react";
 
 interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   children: ReactNode;
 }
 
-export function Link({ children, className = '', ...props }: LinkProps) {
+export function Link({ children, className = "", ...props }: LinkProps) {
   return (
     <a
-      className={`transition-colors duration-200 ${className}`}
+      className={`text-text-white border-b border-border-DEFAULT hover:text-text-300 hover:border-border-strong ${className}`}
       {...props}
     >
       {children}

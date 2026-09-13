0c6d7c5eec69dc6eec6f132bf08e8eb821ee8881
vladyslavspasibozhko <>
2026-09-13 18:28:12 +0200

    refactor: fix ts config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
 import { StarField } from "@components/atoms/StarField";
 import { MainPage } from "./pages/MainPage";
-import { LibraryPage } from "./pages/LibraryPage";
 
 export default function App() {
   return (
     <>
       <StarField />
       <MainPage />
-
-      {/* <LibraryPage /> */}
     </>
   );
 }

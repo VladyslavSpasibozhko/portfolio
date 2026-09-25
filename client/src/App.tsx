cc2cff24bc0c9ed35deeefbc7079d2c5c29cf705
vladyslavspasibozhko <>
2026-09-25 18:54:24 +0200

    feat: hide AI feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,11 @@
 import { StarField } from "@components/atoms/StarField";
-import { ChatWrapper } from "@components/features/chat/ChatWrapper";
 import { MainPage } from "./pages/MainPage";
-import { LibraryPage } from "./pages/LibraryPage";
 
 export default function App() {
   return (
     <>
       <StarField />
       <MainPage />
-      <ChatWrapper />
-      {/* <LibraryPage /> */}
     </>
-  );
+);
 }
